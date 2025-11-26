83c5b99106a492556ddb3212f8806e23b4167c5e
bsainitin <b.sainitin@gmail.com>
2025-11-26 16:27:22 +0530

    Dynamic Count loop to create instances

--- count/ec2.tf
+++ count/ec2.tf
@@ -1,5 +1,6 @@
 resource "aws_instance" "terraform" {
-    count = 10
+    # count = 10
+    count = length(var.instances)
     ami = "ami-09c813fb71547fc4f"
     instance_type = "t3.medium"
     vpc_security_group_ids = [aws_security_group.allow_all.id]
@@ -13,20 +14,20 @@ resource "aws_security_group" "allow_all" {
     name   = "allow-all"
 
     ingress {
-    from_port        = 0 # (from port 0) to (to port 0) means allow all ports
-    to_port          = 0 
-    protocol         = "-1" # -1 means all protocols
-    cidr_blocks      = ["0.0.0.0/0"] # internet 
+        from_port        = 0 # (from port 0) to (to port 0) means allow all ports
+        to_port          = 0 
+        protocol         = "-1" # -1 means all protocols
+        cidr_blocks      = ["0.0.0.0/0"] # internet 
     }
 
     egress {
-    from_port        = 0 
-    to_port          = 0 
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+        from_port        = 0 
+        to_port          = 0 
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
     }
 
     tags = {
-    Name = "allow-all"
+        Name = "allow-all"
     }
 }
