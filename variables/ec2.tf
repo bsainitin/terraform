bb0f06cda67d22dc9fc38f2e6a6a6ddf5371ade2
bsainitin <b.sainitin@gmail.com>
2025-11-26 06:26:31 +0530

    Learn Terraform Variables

---
+++ variables/ec2.tf
@@ -0,0 +1,28 @@
+resource "aws_instance" "terraform" {
+    ami = var.ami_id
+    instance_type = var.instance_type
+    vpc_security_group_ids = [aws_security_group.allow_all.id]
+    tags = var.ec2_tags
+}
+
+resource "aws_security_group" "allow_all" {
+    name   = var.sg_name
+
+    ingress {
+    from_port        = var.ingress_from_port # (from port 0) to (to port 0) means allow all ports
+    to_port          = var.ingress_to_port
+    protocol         = var.protocol # -1 means all protocols
+    cidr_blocks      = var.cidr # internet
+    }
+
+    egress {
+    from_port        = var.egress_from_port
+    to_port          = var.egress_to_port 
+    protocol         = var.protocol
+    cidr_blocks      = var.cidr
+    }
+
+    tags = {
+    Name = "allow-all"
+    }
+}
