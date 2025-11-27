03507d974384eb28cc2ce43c05406a57b5a9097e
bsainitin <b.sainitin@gmail.com>
2025-11-27 14:25:39 +0530

    Create SG with dynamic inbound rules

---
+++ dynamic/sg.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "roboshop-instance" {
+    name   = "roboshop-strict-sg"
+
+    dynamic "ingress" {
+        for_each = toset(var.ingress_ports)
+        content {
+            from_port        = ingress.value
+            to_port          = ingress.value
+            protocol         = "tcp" 
+            cidr_blocks      = ["0.0.0.0/0"]
+        } 
+    }
+
+    egress {
+        from_port        = 0 
+        to_port          = 0 
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "roboshop-strict-sg"
+    }
+}
---
+++ dynamic/variables.tf
@@ -0,0 +1,3 @@
+variable "ingress_ports" {
+    default = [22, 80, 3306, 6379, 27017, 8080]
+}
